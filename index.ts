a6835fa00862bc8e20626b0a8f2d455d6d67b76d
MichaelMadume <m.c.madume@gmail.com>
2023-01-07 18:48:52 +0100

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: { origin: true },
+});
+
+io.on("connection", (socket) => {
+  console.log("New agent connected");
+
+  socket.on("joinRoom", (event) => {
+    socket.join(event);
+  });
+
+  socket.on("listener:camera-feed", (event) => {
+    io.to("controller").emit("listener:camera-feed", event);
+  });
+
+  socket.on("controller:input", (event) => {
+    io.to("listener").emit("controller:input", event);
+  });
+});
+
+httpServer.listen(6900);
+console.log("Listening for requests");
